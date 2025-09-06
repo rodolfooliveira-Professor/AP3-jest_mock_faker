1b6c3b79b7264dda24c55f1c7f55af5b8db02649
rodolfooliveira-Professor <rodolfooliveira@id.uff.br>
2025-09-06 14:46:10 -0300

    Create cardService.js

---
+++ src/cardService.js
@@ -0,0 +1,13 @@
+function evaluateCard(client) {
+  if (client.age < 18) return "NEGADO";
+  if (client.income <= 2000) return "NEGADO";
+  if (client.income > 5000) return "PREMIUM";
+  return "BÁSICO";
+}
+
+// Função de notificação (mockável)
+function sendCardNotification(clientId, cardType) {
+  console.log(`Cliente ${clientId}: Cartão ${cardType} enviado.`);
+}
+
+module.exports = { evaluateCard, sendCardNotification };
