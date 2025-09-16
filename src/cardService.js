77231cd8937021ddcb0b280c124d24326ea2f5e9
Pedro9185 <pedrocatarina.ip@gmail.com>
2025-09-15 21:11:53 -0300

    add: adicionado testes 05 e 06

--- src/cardService.js
+++ src/cardService.js
@@ -8,6 +8,21 @@ function evaluateCard(client) {
 // Função de notificação (mockável)
 function sendCardNotification(clientId, cardType) {
   console.log(`Cliente ${clientId}: Cartão ${cardType} enviado.`);
+
+  // TODO: Verificar as regras de negócio com expect(...)
+  // DONE:Verificar as regras de negócio com expect(...)
+  if (client.age < 18 || client.income <= 2000) {
+    expect(result).toBe("NEGADO");
+  } else if (client.income > 5000) {
+    expect(result).toBe("PREMIUM");
+  } else {
+    expect(result).toBe("BÁSICO");
+  }
+
+  // TODO: Validar se o mock foi chamado corretamente
+  // DONE: Validar se o mock foi chamado corretamente
+  expect(mockNotify).toHaveBeenCalledTimes(1);
+  expect(mockNotify).toHaveBeenCalledWith(client.id, result);
 }
 
 module.exports = { evaluateCard, sendCardNotification };
