ce2b739ac7ffd14f07619db9c462dca3220a0add
lal28 <lucasprolopes@gmail.com>
2025-09-15 21:52:16 -0300

    organização do código

--- test/cardService.test.js
+++ test/cardService.test.js
@@ -3,18 +3,17 @@ const { evaluateCard, sendCardNotification } = require("../src/cardService");
 const { faker } = require("@faker-js/faker");
 
 describe("Testes dinâmicos de liberação de cartão com Faker e Mock", () => {
+
   // TODO: Gerar 100 clientes aleatórios usando faker
   // DONE: Gerar 100 clientes aleatórios usando faker
   const clients = Array.from({ length: 100 }).map(() => ({
     id: faker.string.uuid(), // id único
     age: faker.number.int({ min: 15, max: 80 }), // idade entre 15 e 80
-    income: faker.number.float({ min: 0, max: 10000, precision: 0.01 }), // renda até 10k
+    income: faker.number.float({ min: 0, max: 10000, precision: 0.01 }) // renda até 10k
   }));
 
   clients.forEach((client, index) => {
-    test(`Cliente #${index + 1} -> idade: ${client.age}, renda: ${
-      client.income
-    }`, () => {
+    test(`Cliente #${index + 1} -> idade: ${client.age}, renda: ${client.income}`, () => {
       // TODO: Chamar a função que avalia o cartão
       // DONE:Chamar a função que avalia o cartão
       const result = evaluateCard(client);
@@ -27,10 +26,8 @@ describe("Testes dinâmicos de liberação de cartão com Faker e Mock", () => {
       // DONE:Simular notificação passando client.id e result
       mockNotify(client.id, result);
 
-      // TODO: Chamar o mock passando client.id e result
-
       // TODO: Verificar as regras de negócio com expect(...)
-
+      // DONE:Verificar as regras de negócio com expect(...)
       if (client.age < 18 || client.income <= 2000) {
         expect(result).toBe("NEGADO");
       } else if (client.income > 5000) {
@@ -43,8 +40,7 @@ describe("Testes dinâmicos de liberação de cartão com Faker e Mock", () => {
       // DONE: Validar se o mock foi chamado corretamente
       expect(mockNotify).toHaveBeenCalledTimes(1);
       expect(mockNotify).toHaveBeenCalledWith(client.id, result);
-
-      // TODO: Validar se o mock foi chamado corretamente
     });
   });
+
 });
