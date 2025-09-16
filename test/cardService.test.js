7de1fd98d7dd58992702d60130c38d5565d101d8
lal28 <lucasprolopes@gmail.com>
2025-09-15 21:03:20 -0300

    gerar clientes e avaliar cartao

--- test/cardService.test.js
+++ test/cardService.test.js
@@ -5,35 +5,30 @@ const { faker } = require("@faker-js/faker");
 describe("Testes dinâmicos de liberação de cartão com Faker e Mock", () => {
 
   // TODO: Gerar 100 clientes aleatórios usando faker
+  // DONE: Gerar 100 clientes aleatórios usando faker
   const clients = Array.from({ length: 100 }).map(() => ({
-    id: /* complete */,
-    age: /* complete */,
-    income: /* complete */
+    id: faker.string.uuid(), // id único
+    age: faker.number.int({ min: 15, max: 80 }), // idade entre 15 e 80
+    income: faker.number.float({ min: 0, max: 10000, precision: 0.01 }) // renda até 10k
   }));
 
   clients.forEach((client, index) => {
     test(`Cliente #${index + 1} -> idade: ${client.age}, renda: ${client.income}`, () => {
       // TODO: Chamar a função que avalia o cartão
-      const result = /* complete */;
+      // DONE:Chamar a função que avalia o cartão
+      const result = evaluateCard(client);
 
       // TODO: Criar mock da função de notificação
-      const mockNotify = /* complete */;
+
 
       // TODO: Chamar o mock passando client.id e result
-      /* complete */
+   
 
       // TODO: Verificar as regras de negócio com expect(...)
-      if (/* condição cliente não aprovado */) {
-        expect(result).toBe("NEGADO");
-      } else if (/* condição premium */) {
-        expect(result).toBe("PREMIUM");
-      } else {
-        expect(result).toBe("BÁSICO");
-      }
+     
 
       // TODO: Validar se o mock foi chamado corretamente
-      expect(/* complete */).toHaveBeenCalledTimes(1);
-      expect(/* complete */).toHaveBeenCalledWith(client.id, result);
+      
     });
   });
 
